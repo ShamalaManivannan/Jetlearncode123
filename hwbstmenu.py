1f9662625a46b486641d5921ad9c725b99b10de5
ShamalaManivannan <76401128+ShamalaManivannan@users.noreply.github.com>
2025-03-31 17:10:51 +0200

    Add files via upload

--- hwbstmenu.py
+++ hwbstmenu.py
@@ -30,6 +30,43 @@ def Search(root, key):
         return Search(root.left, key)
     else:
         return Search(root.right, key)
+    
+def Delete(root,key):
+    if root is None:
+        return root
+    if key < root.data:
+        root.left = Delete(root.left,key)
+    elif key > root.data:
+        root.right = Delete(root.right,key) 
+    else:
+        #Node with no child
+        if root.left is None and root.right is None:
+            return None #Return None to indicate that this node should be removed
+        
+        #2nd Scenario = Node with one child
+        elif root.left is None:
+            temp = root.right
+            root = None #removing the root
+            return temp 
+        elif root.right is None:
+            temp = root.left
+            root = None #removing the root 
+            return temp
+    
+        #3rd Scenario = Node with two children
+        temp = minValueNode(root.right)
+        root.data = temp.data
+        root.right = Delete(root.right,temp.data) 
+    
+    return root
+
+def minValueNode(node):
+    current = node
+    while current.left is not None:
+        current = current.left
+    return current
+
+
 
 
 root = None
@@ -39,6 +76,7 @@ while True:
     print("2. Search")
     print("3. Display (InOrder Traversal)")
     print("4. Exit")
+    print("5. Delete")
     
     choice = int(input("Enter your choice: "))
 
@@ -65,5 +103,16 @@ while True:
     elif choice == 4:
         print("Exiting program")
         break
+
+    elif choice == 5:
+        print("BST before deletion:")
+        InOrderTraversal(root)
+        choice = int(input("What number do you want to delete"))
+        root = Delete(root,choice)
+        print("BST after deletion:")
+        InOrderTraversal(root)
+    
     else:
         print("Invalid choice")
+    
+   
